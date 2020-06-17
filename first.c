1940540e022f2935b2e5b2d00b357a7ed683d23b
shahidul-islam125 <58200015+shahidul-islam125@users.noreply.github.com>
2020-06-17 15:02:24 +0600

    Add files via upload

---
+++ first.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("Hello world!");
+    return 0;
+ /* Today's date 16 june 2020
+    from today i will dedicate a lot of time 
+    for achieving my dreams. 
+    Just start NOW! */
+}
